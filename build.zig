679dca1edce5a3498bf5491b0fdcea82322bd8ab
Veikka Tuominen <git@vexu.eu>
2021-12-27 19:15:40 +0200

    update build.zig and basic.zig for 0.9.0

--- build.zig
+++ build.zig
@@ -14,10 +14,10 @@ pub fn build(b: *Builder) void {
     basic.setBuildMode(mode);
     basic.addPackage(.{
         .name = "routez",
-        .path = "src/routez.zig",
+        .path = .{ .path = "src/routez.zig" },
         .dependencies = &[_]std.build.Pkg{.{
             .name = "zuri",
-            .path = "zuri/src/zuri.zig",
+            .path = .{ .path = "zuri/src/zuri.zig" },
         }},
     });
     basic.setOutputDir("zig-cache");
--- examples/basic.zig
+++ examples/basic.zig
@@ -1,46 +1,46 @@
 const std = @import("std");
 const Address = std.net.Address;
-usingnamespace @import("routez");
+const r = @import("routez");
 const allocator = std.heap.page_allocator;
 
 pub const io_mode = .evented;
 
 pub fn main() !void {
-    var server = Server.init(
+    var server = r.Server.init(
         allocator,
         .{},
         .{
-            all("/", indexHandler),
-            get("/about", aboutHandler),
-            get("/about/more", aboutHandler2),
-            get("/post/{post_num}/?", postHandler),
-            static("./", "/static"),
-            all("/counter", counterHandler),
+            r.all("/", indexHandler),
+            r.get("/about", aboutHandler),
+            r.get("/about/more", aboutHandler2),
+            r.get("/post/{post_num}/?", postHandler),
+            r.static("./", "/static"),
+            r.all("/counter", counterHandler),
         },
     );
     var addr = try Address.parseIp("127.0.0.1", 8080);
     try server.listen(addr);
 }
 
-fn indexHandler(req: Request, res: Response) !void {
+fn indexHandler(_: r.Request, res: r.Response) !void {
     try res.sendFile("examples/index.html");
 }
 
-fn aboutHandler(req: Request, res: Response) !void {
+fn aboutHandler(_: r.Request, res: r.Response) !void {
     try res.write("Hello from about\n");
 }
 
-fn aboutHandler2(req: Request, res: Response) !void {
+fn aboutHandler2(_: r.Request, res: r.Response) !void {
     try res.write("Hello from about2\n");
 }
 
-fn postHandler(req: Request, res: Response, args: *const struct {
+fn postHandler(_: r.Request, res: r.Response, args: *const struct {
     post_num: []const u8,
 }) !void {
     try res.print("Hello from post, post_num is {s}\n", .{args.post_num});
 }
 
 var counter = std.atomic.Atomic(usize).init(0);
-fn counterHandler(req: Request, res: Response) !void {
+fn counterHandler(_: r.Request, res: r.Response) !void {
     try res.print("Page loaded {d} times\n", .{counter.fetchAdd(1, .SeqCst)});
 }
